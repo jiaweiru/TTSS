49671dde73e6246cdfd78a8e9f88a02a730e04d0
jiaweiru <13605580719@163.com>
2023-04-13 14:28:02 +0800

    DDP support

--- train_hifigan.py
+++ train_hifigan.py
@@ -332,6 +332,10 @@ if __name__ == "__main__":
     # Load hyperparameters file with command-line overrides
     hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
 
+    # If --distributed_launch then
+    # create ddp_group with the right communication protocol
+    sb.utils.distributed.ddp_init_group(run_opts)
+    
     with open(hparams_file) as fin:
         hparams = load_hyperpyyaml(fin, overrides)
 
